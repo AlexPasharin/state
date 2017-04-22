8d559ca659f4165501779e2098795b295e15ff6d
Aleksandr Pasharin <alex.pasharin@gmail.com>
2017-04-22 14:39:06 +0300

    state back to state

--- src/state/GrownAnimal.java
+++ src/state/GrownAnimal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package visitor;
+package state;
 
 import java.util.Random;
 import java.util.logging.Level;
@@ -13,64 +13,61 @@ import java.util.logging.Logger;
  *
  * @author Alex
  */
-public class ParentAnimal extends AnimalState{
+public class GrownAnimal extends AnimalState {
     
-    private static Random rand = new Random();
+    private static Random rand = new Random();    
     
-    private static ParentAnimal INSTANCE;
+    private static GrownAnimal INSTANCE;
     
-    private ParentAnimal(){}
+    private GrownAnimal(){}
 
    // thread-safe double checking lazy singleton initialization
-    public static ParentAnimal getInstance(){
+    public static GrownAnimal getInstance(){
         if(INSTANCE == null){
-            synchronized(ParentAnimal.class){
+            synchronized(GrownAnimal.class){
                 if(INSTANCE == null){
-                    INSTANCE = new ParentAnimal();
+                    INSTANCE = new GrownAnimal();
                 }               
             }           
         }       
         return INSTANCE;
     }
 
-
-    // because of the parenthood stress parent Animal eats more then he can get energy from 
+    // grown animal eats only if it has food at its store
     @Override
     public void animalEats(Animal animal) {
         if(animal.getAmountOfFood() > 0){
-            animal.addFood(-2);
+            animal.addFood(-1);
             animal.addWeight(1);
         }
-        
     }
-
-    // parent animal has less time to sleep then grown animal
+    
+    // grown animal can only afford to sleep for 100 seconds
     @Override
     public void animalSleeps(Animal animal) {
         animal.setSleeping(true);
         try {
-            Thread.sleep(50000);
+            Thread.sleep(100000);
         } catch (InterruptedException ex) {
             Logger.getLogger(GrownAnimal.class.getName()).log(Level.SEVERE, null, ex);
         }
         animal.setSleeping(false);
     }
 
-    /* When parent hunts half of the food he earns goes to its children 
-        
-    */ 
     @Override
     public void animalHunts(Animal hunter, Animal victum) {
-        
-        if(victum.getSleeping() || rand.nextDouble() > 0.5){ 
-            hunter.addFood(victum.getWeight()/2); // parent gets only half of the animal it killed, rest goes to its children
+        // if victum is sleeping attack always succeeds. otherwise chances are 50/50
+        if(victum.getSleeping() || rand.nextDouble() > 0.5){ // if hunting succeded
+            hunter.addFood(victum.getWeight()); // add the weight of killed animal to hunters food store
             
             // if hunter has enough food at his disposal now, he can mate and gets a new baby
-            if(hunter.getAmountOfFood() > 20 + 10*hunter.getChildren().size()){
+            if(hunter.getAmountOfFood() > 20){
                 hunter.setState(ParentAnimal.getInstance());
                 hunter.getChildren().add(new Animal(hunter)); 
             }
         }
+        
+        
     }
     
 }
--- src/state/ParentAnimal.java
+++ src/state/ParentAnimal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package visitor;
+package state;
 
 import java.util.Random;
 import java.util.logging.Level;
@@ -13,61 +13,64 @@ import java.util.logging.Logger;
  *
  * @author Alex
  */
-public class GrownAnimal extends AnimalState {
+public class ParentAnimal extends AnimalState{
     
-    private static Random rand = new Random();    
+    private static Random rand = new Random();
     
-    private static GrownAnimal INSTANCE;
+    private static ParentAnimal INSTANCE;
     
-    private GrownAnimal(){}
+    private ParentAnimal(){}
 
    // thread-safe double checking lazy singleton initialization
-    public static GrownAnimal getInstance(){
+    public static ParentAnimal getInstance(){
         if(INSTANCE == null){
-            synchronized(GrownAnimal.class){
+            synchronized(ParentAnimal.class){
                 if(INSTANCE == null){
-                    INSTANCE = new GrownAnimal();
+                    INSTANCE = new ParentAnimal();
                 }               
             }           
         }       
         return INSTANCE;
     }
 
-    // grown animal eats only if it has food at its store
+
+    // because of the parenthood stress parent Animal eats more then he can get energy from 
     @Override
     public void animalEats(Animal animal) {
         if(animal.getAmountOfFood() > 0){
-            animal.addFood(-1);
+            animal.addFood(-2);
             animal.addWeight(1);
         }
+        
     }
-    
-    // grown animal can only afford to sleep for 100 seconds
+
+    // parent animal has less time to sleep then grown animal
     @Override
     public void animalSleeps(Animal animal) {
         animal.setSleeping(true);
         try {
-            Thread.sleep(100000);
+            Thread.sleep(50000);
         } catch (InterruptedException ex) {
             Logger.getLogger(GrownAnimal.class.getName()).log(Level.SEVERE, null, ex);
         }
         animal.setSleeping(false);
     }
 
+    /* When parent hunts half of the food he earns goes to its children 
+        
+    */ 
     @Override
     public void animalHunts(Animal hunter, Animal victum) {
-        // if victum is sleeping attack always succeeds. otherwise chances are 50/50
-        if(victum.getSleeping() || rand.nextDouble() > 0.5){ // if hunting succeded
-            hunter.addFood(victum.getWeight()); // add the weight of killed animal to hunters food store
+        
+        if(victum.getSleeping() || rand.nextDouble() > 0.5){ 
+            hunter.addFood(victum.getWeight()/2); // parent gets only half of the animal it killed, rest goes to its children
             
             // if hunter has enough food at his disposal now, he can mate and gets a new baby
-            if(hunter.getAmountOfFood() > 20){
+            if(hunter.getAmountOfFood() > 20 + 10*hunter.getChildren().size()){
                 hunter.setState(ParentAnimal.getInstance());
                 hunter.getChildren().add(new Animal(hunter)); 
             }
         }
-        
-        
     }
     
 }
